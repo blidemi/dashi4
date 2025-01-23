9615b6adee395af72a5a3f9c0f7690fcf203e6b7
Ademi <ademiergasheva@gmail.com>
2025-01-23 20:39:29 +0600

    Add files via upload
    let users = [
     {id: 1, name: "Вася"},
     {id: 2, name: "Петя"},
     {id: 3, name: "Маша"}
    ];
    Найдите пользователя у которого id равен 2.
    Доп задание: Отфильтруйте массив так чтобы в нём остались только те пользователи, у которых id больше 1.

---
+++ hw4/main.js
@@ -0,0 +1,16 @@
+let saram = [
+    { id: 1, name: "Вася" },
+    { id: 2, name: "Петя" },
+    { id: 3, name: "Маша" }
+  ];
+
+let userWithId2 = saram.find(user => user.id === 2);
+
+console.log("Пользователь с id 2:", userWithId2);
+
+
+  
+let filteredSaram = saram.filter(user => user.id > 1);
+
+console.log("Отфильтрованные пользователи:", filteredSaram);
+  
